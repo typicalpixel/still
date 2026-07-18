ff9ad2e667e7b67f188c1592f6ffa220931b6247
Thomas Athanas <thomas@athanasempire.com>
2026-07-18 16:54:04 +0000

    fix assets

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Still.MixProject do
   def project do
     [
       app: :still,
-      version: "0.2.0",
-      elixir: "~> 1.15",
+      version: "0.2.1",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
