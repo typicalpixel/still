47c51ab651a34f0c576e4d29839583738444a7b6
Thomas Athanas <thomas@athanasempire.com>
2026-07-25 13:44:11 +0000

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Still.MixProject do
   def project do
     [
       app: :still,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
